6c1f7ad42205c3a8ac872e65ca02decf83ce8f09
zain shahid <zainulabideenshahid@gmail.com>
2018-01-10 14:03:23 -0500

    added new visualization that changes style of scale and also changed color of bars

--- Airlines/airlines.js
+++ Airlines/airlines.js
@@ -1,120 +1,120 @@
-// Load and munge data, then make the visualization.
-            var fileName = "delays.csv";
-            var nutritionFields = ["AirTran", "Alaska", "American", "Delta", "Endeavor",
-                                   "Envoy", "ExpressJet", "Frontier", "Havaiian", "Jetblue", "SkyWest", "United"];
+var margin = {top: 100, right: 180, bottom: 150, left: 180},
+    width = 1000 - margin.left - margin.right,
+    height = 600 - margin.top - margin.bottom;
 
-            d3.csv(fileName, function(error, data) {
-                var cerealMap = {};
-                data.forEach(function(d) {
-                    var cereal = d.dest;
-                    cerealMap[cereal] = [];
+var svg = d3.select("body").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-                    // { cerealName: [ bar1Val, bar2Val, ... ] }
-                    nutritionFields.forEach(function(field) {
-                        cerealMap[cereal].push( +d[field] );
-                    });
+d3.tsv("delays_1.tsv", function(error, data){
+
+    // filter year
+    var data = data.filter(function(d){return d.Year == '2017';});
+    // Get every column value
+    var elements = Object.keys(data[0])
+        .filter(function(d){
+            return ((d != "Year") & (d != "State"));
+        });
+    var selection = elements[0];
+
+    var y = d3.scale.linear()
+            .domain([0, d3.max(data, function(d){
+                return +d[selection];
+            })])
+            .range([height, 0]);
+
+    var x = d3.scale.ordinal()
+            .domain(data.map(function(d){ return d.State;}))
+            .rangeRoundBands([0, width], 0.1);
+
+
+    var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom");
+
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient("left");
+
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis)
+        .selectAll("text")
+        .style("text-anchor", "end")
+        .attr("dx", "-.8em")
+        .attr("dy", "-.55em")
+        .attr("transform", "rotate(-90)" );
+
+
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis);
+
+    svg.selectAll("rectangle")
+        .data(data)
+        .enter()
+        .append("rect")
+        .attr("class","rectangle")
+        .attr("width", x.rangeBand())
+        .attr("height", function(d){
+            return height - y(+d[selection]);
+        })
+        .attr("x", function(d, i){
+            return x(d.State) ;   
+        })
+        .attr("y", function(d){
+            return y(+d[selection]);
+        })
+        .append("title")
+        .text(function(d){
+            return d.State + " : " + d[selection];
+        });
+
+    var selector = d3.select("#drop")
+        .append("select")
+        .attr("id","dropdown")
+        .on("change", function(d){
+            selection = document.getElementById("dropdown");
+
+            y.domain([0, d3.max(data, function(d){
+                return +d[selection.value];})]);
+
+            yAxis.scale(y);
+
+            d3.selectAll(".rectangle")
+                .transition()
+                .attr("height", function(d){
+                    return height - y(+d[selection.value]);
+                })
+                .attr("x", function(d, i){
+                    return x(d.State) ;
+                })
+                .attr("y", function(d){
+                    return y(+d[selection.value]);
+                })
+                .ease("linear")
+                .select("title")
+                .text(function(d){
+                    return d.State + " : " + d[selection.value];
                 });
-                makeVis(cerealMap);
-            });
+      
+            d3.selectAll("g.y.axis")
+                .transition()
+                .call(yAxis);
 
-            var makeVis = function(cerealMap) {
-                // Define dimensions of vis
-                var margin = { top: 30, right: 50, bottom: 30, left: 50 },
-                    width  = 550 - margin.left - margin.right,
-                    height = 250 - margin.top  - margin.bottom;
+         });
 
-                // Make x scale
-                var xScale = d3.scale.ordinal()
-                    .domain(nutritionFields)
-                    .rangeRoundBands([0, width], 0.1);
+    selector.selectAll("option")
+      .data(elements)
+      .enter().append("option")
+      .attr("value", function(d){
+        return d;
+      })
+      .text(function(d){
+        return d;
+      })
 
-                // Make y scale, the domain will be defined on bar update
-                var yScale = d3.scale.linear()
-                    .range([height, 0]);
-
-                // Create canvas
-                var canvas = d3.select("body")
-                  .append("svg")
-                    .attr("width",  width  + margin.left + margin.right)
-                    .attr("height", height + margin.top  + margin.bottom)
-                  .append("g")
-                    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-                // Make x-axis and add to canvas
-                var xAxis = d3.svg.axis()
-                    .scale(xScale)
-                    .orient("bottom");
-
-                canvas.append("g")
-                    .attr("class", "x axis")
-                    .attr("transform", "translate(0," + height + ")")
-                    .call(xAxis);
-
-                // Make y-axis and add to canvas
-                var yAxis = d3.svg.axis()
-                    .scale(yScale)
-                    .orient("left");
-
-                var yAxisHandleForUpdate = canvas.append("g")
-                    .attr("class", "y axis")
-                    .call(yAxis);
-
-                yAxisHandleForUpdate.append("text")
-                    .attr("transform", "rotate(-90)")
-                    .attr("y", 6)
-                    .attr("dy", ".71em")
-                    .style("text-anchor", "end")
-                    .text("Value");
-
-                var updateBars = function(data) {
-                    // First update the y-axis domain to match data
-                    yScale.domain( d3.extent(data) );
-                    yAxisHandleForUpdate.call(yAxis);
-
-                    var bars = canvas.selectAll(".bar").data(data);
-
-                    // Add bars for new data
-                    bars.enter()
-                      .append("rect")
-                        .attr("class", "bar")
-                        .attr("x", function(d,i) { return xScale( nutritionFields[i] ); })
-                        .attr("width", xScale.rangeBand())
-                        .attr("y", function(d,i) { return yScale(d); })
-                        .attr("height", function(d,i) { return height - yScale(d); });
-
-                    // Update old ones, already have x / width from before
-                    bars
-                        .transition().duration(250)
-                        .attr("y", function(d,i) { return yScale(d); })
-                        .attr("height", function(d,i) { return height - yScale(d); });
-
-                    // Remove old ones
-                    bars.exit().remove();
-                };
-
-                // Handler for dropdown value change
-                var dropdownChange = function() {
-                    var newCereal = d3.select(this).property('value'),
-                        newData   = cerealMap[newCereal];
-
-                    updateBars(newData);
-                };
-
-                // Get names of cereals, for dropdown
-                var cereals = Object.keys(cerealMap).sort();
-
-                var dropdown = d3.select("body")
-                    .insert("select", "svg")
-                    .on("change", dropdownChange);
-
-                dropdown.selectAll("option")
-                    .data(cereals)
-                  .enter().append("option")
-                    .attr("value", function (d) { return d; })
-                    .text(function (d) {
-                        return d[0].toUpperCase() + d.slice(1,d.length); // capitalize 1st letter
-                    });
-
-                var initialData = cerealMap[ cereals[0] ];
-                updateBars(initialData);
-            };
+});
